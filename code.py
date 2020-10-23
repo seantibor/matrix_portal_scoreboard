6ea76e005fda8fbca2cb0916b54058bf8ab61298
Sean Tibor <40579452+seantibor@users.noreply.github.com>
2020-10-23 09:17:45 -0400

    added team color via MQTT

--- code.py
+++ code.py
@@ -25,14 +25,14 @@ mqtt = MQTT.MQTT(
 
 MQTT.set_socket(socket, network._wifi.esp)
 
-RED_COLOR = 0x00AA00
-BLUE_COLOR = 0xAAAAAA
+TEAM_1_COLOR = 0x00AA00
+TEAM_2_COLOR = 0xAAAAAA
 
 # Red Score
 matrixportal.add_text(
     text_font=terminalio.FONT,
     text_position=(4, int(matrixportal.graphics.display.height * 0.75) - 3),
-    text_color=RED_COLOR,
+    text_color=TEAM_1_COLOR,
     text_scale=2,
 )
 
@@ -40,7 +40,7 @@ matrixportal.add_text(
 matrixportal.add_text(
     text_font=terminalio.FONT,
     text_position=(36, int(matrixportal.graphics.display.height * 0.75) - 3),
-    text_color=BLUE_COLOR,
+    text_color=TEAM_2_COLOR,
     text_scale=2,
 )
 
@@ -48,14 +48,14 @@ matrixportal.add_text(
 matrixportal.add_text(
     text_font=terminalio.FONT,
     text_position=(4, int(matrixportal.graphics.display.height * 0.25) - 4),
-    text_color=RED_COLOR,
+    text_color=TEAM_1_COLOR,
 )
 
 # Blue Team name
 matrixportal.add_text(
     text_font=terminalio.FONT,
     text_position=(36, int(matrixportal.graphics.display.height * 0.25) - 4),
-    text_color=BLUE_COLOR,
+    text_color=TEAM_2_COLOR,
 )
 
 # Static 'Connecting' Text
@@ -64,17 +64,19 @@ matrixportal.add_text(
     text_position=(59, 0),
 )
 
-SCORES_RED_FEED = "homeassistant/findeiss/1/dispenses/today"
-SCORES_BLUE_FEED = "homeassistant/findeiss/1/dispenses/yesterday"
-TEAM_RED_FEED = "homeassistant/findeiss/1/dispenses/today/label"
-TEAM_BLUE_FEED = "homeassistant/findeiss/1/dispenses/yesterday/label"
+SCORES_1_FEED = "homeassistant/findeiss/1/dispenses/today"
+SCORES_2_FEED = "homeassistant/findeiss/1/dispenses/yesterday"
+TEAM_1_FEED = "homeassistant/findeiss/1/dispenses/today/label"
+TEAM_2_FEED = "homeassistant/findeiss/1/dispenses/yesterday/label"
+TEAM_1_COLOR_FEED = "homeassistant/findeiss/1/dispenses/today/color"
+TEAM_2_COLOR_FEED = "homeassistant/findeiss/1/dispenses/yesterday/color"
 
 last_data = {}
 
 UPDATE_DELAY = 4
 
-matrixportal.set_text_color(RED_COLOR, 0)
-matrixportal.set_text_color(BLUE_COLOR, 1)
+matrixportal.set_text_color(TEAM_1_COLOR, 0)
+matrixportal.set_text_color(TEAM_2_COLOR, 1)
 
 
 def show_connecting(show):
@@ -96,20 +98,37 @@ def get_last_data(feed):
 
 
 def customize_team_names():
-    team_red = "Red"
-    team_blue = "Blue"
+    team_1 = "Red"
+    team_2 = "Blue"
+
+    global TEAM_1_COLOR
+    global TEAM_2_COLOR
 
     show_connecting(True)
-    team_name = get_last_data(TEAM_RED_FEED)
+    team_name = get_last_data(TEAM_1_FEED)
     if team_name is not None:
-        print("Team {} is now Team {}".format(team_red, team_name))
-        team_red = team_name
-    matrixportal.set_text(team_red, 2)
-    team_name = get_last_data(TEAM_BLUE_FEED)
+        print("Team {} is now Team {}".format(team_1, team_name))
+        team_1 = team_name
+    matrixportal.set_text(team_1, 2)
+    team_color = get_last_data(TEAM_1_COLOR_FEED)
+    if team_color is not None:
+        team_color = int(team_color.replace("#", "").strip(), 16)
+        print("Team {} is now Team {}".format(team_1, team_color))
+        TEAM_1_COLOR = team_color
+    matrixportal.set_text_color(TEAM_1_COLOR, 2)
+    matrixportal.set_text_color(TEAM_1_COLOR, 0)
+    team_name = get_last_data(TEAM_2_FEED)
     if team_name is not None:
-        print("Team {} is now Team {}".format(team_blue, team_name))
-        team_blue = team_name
-    matrixportal.set_text(team_blue, 3)
+        print("Team {} is now Team {}".format(team_2, team_name))
+        team_2 = team_name
+    matrixportal.set_text(team_2, 3)
+    team_color = get_last_data(TEAM_2_COLOR_FEED)
+    if team_color is not None:
+        team_color = int(team_color.replace("#", "").strip(), 16)
+        print("Team {} is now Team {}".format(team_2, team_color))
+        TEAM_2_COLOR = team_color
+    matrixportal.set_text_color(TEAM_2_COLOR, 3)
+    matrixportal.set_text_color(TEAM_2_COLOR, 1)
     show_connecting(False)
 
 
@@ -117,12 +136,12 @@ def update_scores():
     print("Updating data from Adafruit IO")
     show_connecting(True)
 
-    score_red = get_last_data(SCORES_RED_FEED)
+    score_red = get_last_data(SCORES_1_FEED)
     if score_red is None:
         score_red = 0
     matrixportal.set_text(score_red, 0)
 
-    score_blue = get_last_data(SCORES_BLUE_FEED)
+    score_blue = get_last_data(SCORES_2_FEED)
     if score_blue is None:
         score_blue = 0
     matrixportal.set_text(score_blue, 1)
@@ -135,10 +154,12 @@ def subscribe():
     except MQTT.MMQTTException:
         mqtt.connect()
     mqtt.on_message = message_received
-    mqtt.subscribe(SCORES_BLUE_FEED)
-    mqtt.subscribe(SCORES_RED_FEED)
-    mqtt.subscribe(TEAM_BLUE_FEED)
-    mqtt.subscribe(TEAM_RED_FEED)
+    mqtt.subscribe(SCORES_2_FEED)
+    mqtt.subscribe(SCORES_1_FEED)
+    mqtt.subscribe(TEAM_2_FEED)
+    mqtt.subscribe(TEAM_1_FEED)
+    mqtt.subscribe(TEAM_2_COLOR_FEED)
+    mqtt.subscribe(TEAM_1_COLOR_FEED)
 
 
 subscribe()
