8d7a0c21082092e757b36d200d5fa197271a3751
Sean Tibor <40579452+seantibor@users.noreply.github.com>
2020-10-26 07:39:18 -0400

    added secrets example

---
+++ secrets-example.py
@@ -0,0 +1,11 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+ 
+secrets = {
+    'ssid' : '## REPLACE ##',
+    'password' : '## REPLACE ##',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'mqtt_broker': '## REPLACE ##',
+    'mqtt_user': '## REPLACE ##',
+    'mqtt_password': '## REPLACE ##',
+    }
